5c7f62909541a51994e816d22833158b496a950c
Kasper Radom <radom.kasper@gmail.com>
2023-06-21 00:05:35 +0200

    Działa mnożenie w verilogu!!!

--- python/ Modulus function computation/modulo.py
+++ python/ Modulus function computation/modulo.py
@@ -3,7 +3,6 @@ import math
 x=9876543210
 p=17
 
-reszta = x%p
 x=bin(x)[2:]
 
 n=len(x)
@@ -18,20 +17,15 @@ xList=[]
 for i in range(1, k + 1):
     xList.append(x[len(x) - i * r:len(x) - (i - 1) * r])
 
-print("x =",x)
-print("X1,X2,... =",xList)
 
 s=0
 for i in range(1,k+1):
     s+=int(xList[i-1],2)*(pow(2,r*(i-1))%p)
 
-print("binary s",bin(s)[2:])
 
 s_temp=s
-print("s_temp=", s_temp, "2p=",2*p)
-print("")
-while s_temp>=2*p:
-    print("--------------------------Pętla sie wykonała")
+
+while s_temp>=2*p: #tu norówność ostra została zmieniona na nieostrą z podowu błędu w pseudokodzie
     s_temp = bin(s_temp)[2:]
     n_temp=len(s_temp)
     k_temp=math.ceil(n_temp/r)
@@ -46,7 +40,6 @@ while s_temp>=2*p:
     newS_temp=0
     for i in range(1,k_temp+1):
         newS_temp+=int(sList[i-1],2)*(pow(2,r*(i-1))%p)
-    print("s_temp",int(s_temp,2))
     s_temp=newS_temp
 
 if p<=s_temp:
@@ -54,10 +47,4 @@ if p<=s_temp:
 else:
     s=s_temp
 
-print("s                =",s)
-print("Prawidlowy wynik =",reszta)
-
-if reszta!=s:
-    print("WYNIK SIE NIE ZGADZA")
-else:
-    print("WYNIK SIE ZGADZA")
+print("s=",s)
--- python/moduloMultiplication/mulMod.py
+++ python/moduloMultiplication/mulMod.py
@@ -1,9 +1,7 @@
 
 a = 53
 b = 19
-p = 47
 
-print("a= ", a, "b= ", b)
 
 a = bin(a)[2:]
 b = bin(b)[2:]
@@ -23,12 +21,11 @@ if len(b) < 6:
 a = list(a)
 b = list(b)
 
-print("a= ", a, "b= ", b)
 
 label1 = int(int(''.join(map(str, a[3:6])), 2)) * int(int(''.join(map(str, b[3:6])), 2))
-label2 = (int(int(''.join(map(str, a[3:6])), 2)) * int(int(''.join(map(str, b[:3])), 2)) * 8) % p
-label3 = (int(int(''.join(map(str, a[:3])), 2)) * int(int(''.join(map(str, b[3:6])), 2)) * 8) % p
-label4 = (int(int(''.join(map(str, a[:3])), 2)) * int(int(''.join(map(str, b[:3])), 2)) * 17) % p
+label2 = (int(int(''.join(map(str, a[3:6])), 2)) * int(int(''.join(map(str, b[:3])), 2)) * 8) % 47
+label3 = (int(int(''.join(map(str, a[:3])), 2)) * int(int(''.join(map(str, b[3:6])), 2)) * 8) % 47
+label4 = (int(int(''.join(map(str, a[:3])), 2)) * int(int(''.join(map(str, b[:3])), 2)) * 17) % 47
 
 temp_R_1 = label1 + label2 + label3 + label4
 temp_R_1 = bin(temp_R_1)[2:]
@@ -36,13 +33,13 @@ temp_R_1 = bin(temp_R_1)[2:]
 if len(temp_R_1) < 8:
     temp_R_1 = (8 - len(temp_R_1)) * "0" + temp_R_1
 
-label5 = (int(''.join(map(str, temp_R_1[2:5])), 2) * 8) % p
-label6 = (int(''.join(map(str, temp_R_1[:2])), 2) * 17) % p
+label5 = (int(''.join(map(str, temp_R_1[2:5])), 2) * 8) % 47
+label6 = (int(''.join(map(str, temp_R_1[:2])), 2) * 17) % 47
 
 temp_R_2 = int(temp_R_1[5:], 2) + label5 + label6
 
-if temp_R_2 >= p:
-    temp_R = temp_R_2 - p
+if temp_R_2 >= 47:
+    temp_R = temp_R_2 - 47
 else:
     temp_R = temp_R_2
 
@@ -50,11 +47,4 @@ R = temp_R
 
 print("R =", R)
 
-a = ''.join(str(bit) for bit in a)
-a = int(a, 2)
-
-b = ''.join(str(bit) for bit in b)
-b = int(b, 2)
-
-print("Poprawny wynik:", (a * b) % p)
 
